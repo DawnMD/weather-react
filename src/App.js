64710d069e906c43dcd46608c9aa291e3f0c4a0e
Mainak Das <mainakdas104@gmail.com>
2021-02-07 15:07:13 +0530

    constructor, state init

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from "react";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { lat: null };
+  }
   render() {
     window.navigator.geolocation.getCurrentPosition(
       (position) => console.log(position),
