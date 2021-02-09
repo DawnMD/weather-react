e01f4fbd47bf36897a2dde73a74be1b3b4ffa0b2
Mainak Das <mainakdas104@gmail.com>
2021-02-09 12:41:34 +0530

    functional -> class based component

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,13 +1,26 @@
-import React from "react"
+import React, {Component} from "react"
 
-const Loader = (props) =>{
-    // console.log(props)
-    return (
-    <div className="ui active dimmer">
-        <div className="ui text massive loader">{props.children}</div>
-    </div>
-    )
+// const Loader = (props) =>{
+//     // console.log(props)
+//     return (
+//     <div className="ui active dimmer">
+//         <div className="ui text massive loader">{props.children}</div>
+//     </div>
+//     )
+// }
+class Loader extends Component {
+    constructor(props){
+        super(props)
+    }
+    render(){
+        return(
+        <div className="ui active dimmer">
+            <div className="ui text massive loader">{this.props.children}</div>
+        </div>
+        );
+    }
 }
+
 Loader.defaultProps = {
     children: "Loading..."
 }
--- src/components/SeasonDisplay.jsx
+++ src/components/SeasonDisplay.jsx
@@ -1,35 +1,69 @@
-import React from "react"
+import React, {Component} from "react"
 import "./css/SeasonDisplay.css"
 
-const seasonConfig = {
-    summer : {
-        text: "Lets hit the pool",
-        iconName : "sun"
-    },
-    winter : {
-        text: "Burr, it is chilly outside",
-        iconName : "snowflake"
+//Class based Component
+class SeasonDisplay extends Component{
+    constructor(props){
+        super(props)
+        this.seasonConfig = {
+            summer : {
+                text: "Lets hit the pool",
+                iconName : "sun"
+            },
+            winter : {
+                text: "Burr, it is chilly outside",
+                iconName : "snowflake"
+            }
+        }
+    }
+    getSeason = (lat,month) => {
+        if(month > 2 && month < 9){
+            return lat > 0 ? "summer" : "winter";
+        } else {
+            return lat > 0 ? "winter" : "summer";
+        }
+    }
+    render(){
+        const season = this.getSeason(this.props.lat, new Date().getMonth())
+        const {text, iconName} = this.seasonConfig[season]
+        return(
+            <div className={`season-display ${season}`}>
+                <i className={`icon-left massive ${iconName} icon`}></i> 
+                <h1>{text}</h1> 
+                <i className={`icon-right massive ${iconName} icon`}></i> 
+            </div>
+        )
     }
 }
+//Funtional Component
+// const seasonConfig = {
+//     summer : {
+//         text: "Lets hit the pool",
+//         iconName : "sun"
+//     },
+//     winter : {
+//         text: "Burr, it is chilly outside",
+//         iconName : "snowflake"
+//     }
+// }
 
-const getSeason = (lat,month) => {
-    if(month > 2 && month < 9){
-        return lat > 0 ? "summer" : "winter";
-    } else {
-        return lat > 0 ? "winter" : "summer";
-    }
-}
+// const getSeason = (lat,month) => {
+//     if(month > 2 && month < 9){
+//         return lat > 0 ? "summer" : "winter";
+//     } else {
+//         return lat > 0 ? "winter" : "summer";
+//     }
+// }
 
-const SeasonDisplay = (props) => {
-    const season = getSeason(props.lat, new Date().getMonth())
-    const {text, iconName} = seasonConfig[season]
-    return (
-        <div className={`season-display ${season}`}>
-            <i className={`icon-left massive ${iconName} icon`}></i> 
-            <h1>{text}</h1> 
-            <i className={`icon-right massive ${iconName} icon`}></i> 
-        </div>
-    )
-}
-
+// const SeasonDisplay = (props) => {
+//     const season = getSeason(props.lat, new Date().getMonth())
+//     const {text, iconName} = seasonConfig[season]
+//     return (
+//         <div className={`season-display ${season}`}>
+//             <i className={`icon-left massive ${iconName} icon`}></i> 
+//             <h1>{text}</h1> 
+//             <i className={`icon-right massive ${iconName} icon`}></i> 
+//         </div>
+//     )
+// }
 export default SeasonDisplay;
